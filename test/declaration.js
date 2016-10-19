06f823024a17ab8f0e0a573925fce00ad018220d
Cory Reed <creed@mrn.org>
2016-10-19 12:23:42 -0700

    Add FreeSurfer mocks, test declaration to project.

--- test/declaration.js
+++ test/declaration.js
@@ -2,12 +2,51 @@
 
 const path = require('path');
 
+const mocksPath = path.join(__dirname, 'mocks');
+
 module.exports = {
   computationPath: path.resolve(__dirname, '../src/index.js'),
-  local: [{
-    dirs: [path.join(__dirname, 'site1')],
-  }, {
-    dirs: [path.join(__dirname, 'site2')],
-  }],
+  local: [
+    {
+      files: [
+        {
+          filename: path.join(mocksPath, 'M1.txt'),
+        },
+        {
+          filename: path.join(mocksPath, 'M2.txt'),
+        },
+      ],
+      metaCovariateMapping: {
+        '2': 0,
+        '3': 1,
+      },
+      metaFile: [
+        ['filename', 'is control', 'age'],
+        ['M1.txt', '1', '36'],
+        ['M2.txt', '0', '55'],
+      ],
+      name: 'project-1',
+    },
+    {
+      files: [
+        {
+          filename: path.join(mocksPath, 'M3.txt'),
+        },
+        {
+          filename: path.join(mocksPath, 'M4.txt'),
+        },
+      ],
+      metaCovariateMapping: {
+        '2': 1,
+        '3': 0,
+      },
+      metaFile: [
+        ['File', 'Age', 'IsControl'],
+        ['M3.txt', '27', true],
+        ['M4.txt', '48', false],
+      ],
+      name: 'project-2',
+    },
+  ],
   verbose: true,
 };
