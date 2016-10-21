232e8435d70c41c0a56d82b853e9f7e0710a8ee8
Cory Reed <creed@mrn.org>
2016-10-21 12:17:00 -0700

    Remove unnecessary strings from object keys.

--- test/declaration.js
+++ test/declaration.js
@@ -17,8 +17,8 @@ module.exports = {
         },
       ],
       metaCovariateMapping: {
-        '2': 0,
-        '3': 1,
+        2: 0,
+        3: 1,
       },
       metaFile: [
         ['filename', 'is control', 'age'],
@@ -37,8 +37,8 @@ module.exports = {
         },
       ],
       metaCovariateMapping: {
-        '2': 1,
-        '3': 0,
+        2: 1,
+        3: 0,
       },
       metaFile: [
         ['File', 'Age', 'IsControl'],
