MRN-Code/decentralized-single-shot-ridge-regression

commit ab818c7d17fe745070725181d577d3f6d628d8b3

Move 2nd local fn into 1st.

src/index.js
@@ -86,10 +86,35 @@ module.exports = {
       const pickFeature = getFreeSurferDataPicker(features[0]);
 
       return Promise.all(files.map(f => readFileAsync(f.filename)))
-        .then(freeSurferDatas => ({
-          X: files.map(getNormalizedTags), // covariates
-          y: freeSurferDatas.map(pickFeature), // RoIs
-        }));
+        .then(freeSurferDatas => { // eslint-disable-line arrow-parens
+          const x = files.map(getNormalizedTags); // covariates
+          const y = freeSurferDatas.map(pickFeature); // RoIs
+
+          // calculate regression result and localMeanY
+          const localCount = y.length;
+          const betaVector = regression.oneShot(x, y);
+          const rSquared = regression.rSquared(x, y, betaVector);
+          const tValue = regression.tValue(x, y, betaVector);
+          const localMeanY = n.sum(y) / localCount;
+
+          /* eslint-disable no-console */
+          console.log('X is:', x);
+          console.log('y is:', y);
+          console.log('beta vector is:', betaVector);
+          console.log('local r square of fitting is:', rSquared);
+          console.log('local tValues of betaVector are:', tValue);
+          /* eslint-enable no-console */
+
+          return {
+            betaVector,
+            localCount,
+            localMeanY,
+            rSquared,
+            tValue,
+            x,
+            y,
+          };
+        });
     },
     inputs: [{
       help: 'Select Freesurfer region(s) of interest',
@@ -101,31 +126,7 @@ module.exports = {
       type: 'covariates',
     }],
   }, {
-    // step one: calculate regression result and localMeanY
-    type: 'function',
-    fn(opts) {
-      const previousData = opts.previousData;
-
-      for (let i = 0; i < opts.previousData.X.length; i += 1) {
-        previousData.X[i].splice(0, 0, 1);
-      }
-
-      const betaVector = regression.oneShot(previousData.X, previousData.y);
-      const rSquared = regression.rSquared(previousData.X, previousData.y, betaVector);
-      const tValue = regression.tValue(previousData.X, previousData.y, betaVector);
-      const localMeanY = n.sum(previousData.y) / previousData.y.length;
-      /* eslint-disable no-console */
-      console.log('X is:', previousData.X);
-      console.log('y is:', previousData.y);
-      console.log('beta vector is:', betaVector);
-      console.log('local r square of fitting is:', rSquared);
-      console.log('local tValues of betaVector are:', tValue);
-      /* eslint-enable no-console */
-      return { betaVector, localMeanY, local_n: previousData.y.length };
-    },
-
-  }, {
-     // step two: receive the globalMeanY and calculate part of (y-globalMeanY).^2
+    // step two: receive the globalMeanY and calculate part of (y-globalMeanY).^2
     type: 'function',
     fn(opts) {
       const remoteResults = opts.remoteResults;

test/index.js
@@ -61,7 +61,7 @@ tape('local preprocessing: errors', t => {
 });
 
 tape('local preprocessing', t => {
-  t.plan(2);
+  t.plan(6);
 
   preprocess({
     remoteResult: {
@@ -86,8 +86,15 @@ tape('local preprocessing', t => {
     },
   })
     .then(response => {
+      t.equal(response.localCount, 2, 'returns count');
+      t.equal(response.localMeanY, 4150, 'returns mean RoI');
+      t.ok(typeof response.rSquared === 'number', 'returns r^2');
+      t.ok(
+        Array.isArray(response.tValue) && response.tValue.length,
+        'returns t-value'
+      );
       t.deepEqual(
-        response.X,
+        response.x,
         [
           [29, 1],
           [30, -1],

commit d37f2eb44c3bd4acf8b40a14fc00fc738f6bf06e

Add bias to covariates.
This fixes a regression introduced in ab818c7d17fe745070725181d577d3f6d628d8b3
where covariate bias was dropped.

src/index.js
@@ -91,14 +91,15 @@ module.exports = {
           const y = freeSurferDatas.map(pickFeature); // RoIs
 
           // calculate regression result and localMeanY
+          const biasedX = x.map(covariates => [1].concat(covariates));
           const localCount = y.length;
-          const betaVector = regression.oneShot(x, y);
-          const rSquared = regression.rSquared(x, y, betaVector);
-          const tValue = regression.tValue(x, y, betaVector);
+          const betaVector = regression.oneShot(biasedX, y);
+          const rSquared = regression.rSquared(biasedX, y, betaVector);
+          const tValue = regression.tValue(biasedX, y, betaVector);
           const localMeanY = n.sum(y) / localCount;
 
           /* eslint-disable no-console */
-          console.log('X is:', x);
+          console.log('X is:', biasedX);
           console.log('y is:', y);
           console.log('beta vector is:', betaVector);
           console.log('local r square of fitting is:', rSquared);