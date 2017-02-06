00510fb15134a87efd29c6f03cffd9fab170164e
Cory Reed <creed@mrn.org>
2017-02-06 14:39:21 -0800

    Refactor simulation test.
    This adds enough test data files to make Student t distribution work.
    It also refactors the declaration file to be dynamic.

--- test/declaration.js
+++ test/declaration.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { DEFAULT_LAMBDA, DECLARATION_INPUTS_KEY } = require('../src/constants.js');
+const random = require('lodash/random');
 const path = require('path');
 
 const inputs = [[['Left-Hippocampus'], DEFAULT_LAMBDA]];
@@ -9,76 +10,17 @@ const mocksPath = path.join(__dirname, 'mocks');
 module.exports = {
   computationPath: path.resolve(__dirname, '../src/index.js'),
   local: [
-    {
-      [DECLARATION_INPUTS_KEY]: inputs,
-      files: [
-        {
-          filename: path.join(mocksPath, 'M1.txt'),
-          tags: {
-            isControl: true,
-            age: 36,
-          },
-        },
-        {
-          filename: path.join(mocksPath, 'M2.txt'),
-          tags: {
-            isControl: false,
-            age: 35,
-          },
-        },
-        {
-          filename: path.join(mocksPath, 'M3.txt'),
-          tags: {
-            isControl: true,
-            age: 50,
-          },
-        },
-        {
-          filename: path.join(mocksPath, 'M4.txt'),
-          tags: {
-            isControl: false,
-            age: 18,
-          },
-        },
-
-      ],
-      name: 'project-1',
-    },
-    {
-      [DECLARATION_INPUTS_KEY]: inputs,
-      files: [
-        {
-          filename: path.join(mocksPath, 'M1.txt'),
-          tags: {
-            isControl: true,
-            age: 27,
-          },
-        },
-        {
-          filename: path.join(mocksPath, 'M2.txt'),
-          tags: {
-            isControl: false,
-            age: 56,
-          },
-        },
-        {
-          filename: path.join(mocksPath, 'M3.txt'),
-          tags: {
-            isControl: true,
-            age: 38,
-          },
-        },
-        {
-          filename: path.join(mocksPath, 'M4.txt'),
-          tags: {
-            isControl: false,
-            age: 49,
-          },
-        },
-
-      ],
-      name: 'project-2',
-    },
-  ],
+    ['M1.txt', 'M2.txt', 'M3.txt', 'M4.txt'],
+    ['M5.txt', 'M6.txt', 'M7.txt', 'M8.txt'],
+  ].map(files => ({
+    [DECLARATION_INPUTS_KEY]: inputs,
+    files: files.map(file => ({
+      filename: path.join(mocksPath, file),
+      tags: {
+        age: random(18, 65),
+        isControl: Math.random() > 0.5,
+      },
+    })),
+  })),
   verbose: true,
 };
