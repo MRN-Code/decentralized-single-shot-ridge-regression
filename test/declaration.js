b5ae3d0c213a53b705ae01f1884e2643cf137b7b
Cory Reed <creed@mrn.org>
2017-02-28 11:15:24 -0800

    Update declaration with required metadata and inputs.
    This makes the declaration work the newly required covariates and
    computation inputs. See MRN-Code/coinstac#159

--- test/declaration.js
+++ test/declaration.js
@@ -1,26 +1,36 @@
 'use strict';
 
-const { DEFAULT_LAMBDA, DECLARATION_INPUTS_KEY } = require('../src/constants.js');
-const random = require('lodash/random');
+const { DEFAULT_LAMBDA } = require('../src/constants.js');
 const path = require('path');
 
-const inputs = [[['Left-Hippocampus'], DEFAULT_LAMBDA]];
-const mocksPath = path.join(__dirname, 'mocks');
-
 module.exports = {
+  /**
+   * This property is used to pass computation input values from the
+   * declaration into the computation.
+   *
+   * @todo Don't require `covariates` computation input
+   *
+   * {@link https://github.com/MRN-Code/coinstac/issues/161}
+   */
+  __ACTIVE_COMPUTATION_INPUTS__: [[
+    ['TotalGrayVol'], // FreeSurfer region of interest
+    DEFAULT_LAMBDA, // Lambda
+    [{
+      name: 'Is Control',
+      type: 'boolean',
+    }],
+  ]],
   computationPath: path.resolve(__dirname, '../src/index.js'),
-  local: [
-    ['M1.txt', 'M2.txt', 'M3.txt', 'M4.txt'],
-    ['M5.txt', 'M6.txt', 'M7.txt', 'M8.txt'],
-  ].map(files => ({
-    [DECLARATION_INPUTS_KEY]: inputs,
-    files: files.map(file => ({
-      filename: path.join(mocksPath, file),
-      tags: {
-        age: random(18, 65),
-        isControl: Math.random() > 0.5,
-      },
-    })),
-  })),
+  local: [{
+    metaFilePath: path.join(__dirname, 'mocks/metadata-1.csv'),
+    metaCovariateMapping: {
+      1: 0,
+    },
+  }, {
+    metaFilePath: path.join(__dirname, 'mocks/metadata-2.csv'),
+    metaCovariateMapping: {
+      1: 0,
+    },
+  }],
   verbose: true,
 };
